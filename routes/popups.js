23dc372fb64d7d460a162cc9f0371ec16b0c4d3f
Topher Johnson <wolfthejohnson@gmail.com>
2021-05-29 01:04:48 -0500

    stuff

--- routes/popups.js
+++ routes/popups.js
@@ -41,4 +41,24 @@ router.get('/', ensureAuth, async (req, res) => {
     }
   })
 
+// @desc Show edit page
+// @route GET /popups/edit/:d
+router.get('/edit/:id', ensureAuth, async (req, res) => {
+  const popup = await Popup.findOne({
+    _id: req.params.id
+  }).lean()
+
+  if(!story) {
+    return res.render('error/404')
+  }
+
+  if(story.user != req.user.id) {
+    res.redirect('/popups')
+  } else{
+    res.render('/popups/edit', {
+      popup,
+    })
+  }
+})
+
 module.exports = router
